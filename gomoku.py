1e1df361b03a7f8b3b801a111a83a80c6ce63075
Usui Kakeru <kakeru3.141592@gmail.com>
2018-02-11 21:57:02 +0900

    先行後攻のラジオボタンとスタートボタン追加

--- gomoku.py
+++ gomoku.py
@@ -11,7 +11,27 @@ root.minsize(660,480)
 root.option_add('*font', ('FixedSys', 14))
 
 #ラベル
-Static1 = Tkinter.Label(text=u'五目並べ')
-Static1.pack()
+Label1 = Tkinter.Label(text=u'五目並べ')
+Label1.pack()
+
+var = Tkinter.StringVar()
+var.set('koukou')
+
+def setSenkou():
+	var.set('senkou')
+
+def setKoukou():
+	var.set('koukou')
+
+def getTurnNumber():
+	print var.get()
+
+# ラジオボタンの設定
+for txt,com,x,y in [(u'先行',setSenkou,220,30), (u'後攻',setKoukou,280,30)]:
+    Tkinter.Radiobutton(root, text = txt,value=txt,command=com).place(x=x,y=y)
+
+# ボタン
+startButton = Tkinter.Button(root, text = 'スタート',command = getTurnNumber)
+startButton.place(x=350,y=28)
 
 root.mainloop()
