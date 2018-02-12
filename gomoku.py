e6b9eec952e195876fd2de16e3afc46ce7bfcc86
Usui Kakeru <kakeru3.141592@gmail.com>
2018-02-12 12:31:08 +0900

    ボタン押したらマルバツ表示できるように

--- gomoku.py
+++ gomoku.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 import sys
-import Tkinter
+import Tkinter as tk
 
 
-root = Tkinter.Tk()
+root = tk.Tk()
 root.title(u'五目並べ')
-root.geometry("300x480+100+100")#ヨコ＊タテ+X+Y
-root.minsize(300,480)
+root.geometry("+300+300")#ヨコ＊タテ+X+Y
+# root.resizable(width=False, height=False)
 root.option_add('*font', ('FixedSys', 14))
 
-var = Tkinter.StringVar()
+var = tk.StringVar()
 var.set('koukou')
+
+isSenkou = tk.BooleanVar()
+isSenkou.set(True)
+
+isStart = tk.BooleanVar()
+isStart.set(False)
 
 def setSenkou():
 	var.set('senkou')
@@ -19,27 +25,50 @@ def setSenkou():
 def setKoukou():
 	var.set('koukou')
 
-def getTurnNumber():
+def start():
+	isStart.set(True)
 	print var.get()
 	startButton.configure(state='disabled')
-	gomokuFrame.pack(fill=Tkinter.BOTH)
 
-settingFrame = Tkinter.Frame(root)
+
+def pushButton(event):
+	if isStart.get():
+		if isSenkou.get():
+			event.widget['text'] = '◯'
+			isSenkou.set(False)
+		else:
+			event.widget['text'] = '✕'
+			isSenkou.set(True)
+
+
+settingFrame = tk.Frame(root)
 # ラジオボタン
 for txt,com in [(u'先行',setSenkou), (u'後攻',setKoukou)]:
-    Tkinter.Radiobutton(settingFrame, text = txt,value=txt,command=com).pack()
+    tk.Radiobutton(settingFrame, text = txt,value=txt,command=com).pack()
 
 # スタートボタン
-startButton = Tkinter.Button(settingFrame, text = u'スタート',command = getTurnNumber)
+startButton = tk.Button(settingFrame, text = u'スタート',command = start)
 startButton.pack()
 
-settingFrame.pack()
-gomokuFrame = Tkinter.Frame(root)
+settingFrame.pack(padx=100)
 
-button = Tkinter.Button(gomokuFrame, text = '00').grid(column=0,row=0)
-button = Tkinter.Button(gomokuFrame, text = '00').grid(column=1,row=0)
-button = Tkinter.Button(gomokuFrame, text = '00').grid(column=2,row=0)
-button = Tkinter.Button(gomokuFrame, text = '00').grid(column=3,row=0)
-button = Tkinter.Button(gomokuFrame, text = '00').grid(column=4,row=0)
+gomokuFrame = tk.Frame(root)
+gomokuFrame.pack()
+
+
+
+banmen=[[],[],[],[],[]]
+
+for col in range(5):
+	for row in range(5):
+		button = tk.Button(gomokuFrame,text='　')
+		button.bind('<Button-1>',pushButton)
+		button.grid(column=col,row=row)
+		banmen[col].append(button)
+
+for aaa in range(5):
+	for bbb in range(5):
+		print banmen[aaa][bbb]['text']
+
 
 root.mainloop()
